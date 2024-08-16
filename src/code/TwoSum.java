f2b1fc5b0140e3098d4ddb6e54cbe991e23c49a2
Kolade-Amire <stephamire@gmail.com>
2024-08-16 17:03:02 +0100

    binary search

--- src/code/TwoSum.java
+++ src/code/TwoSum.java
@@ -1,4 +1,4 @@
-package code.two_sum;
+package code;
 
 /* QUESTION COPIED FROM LEETCODE (https://leetcode.com/problems/two-sum/)
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
